7d428a07a318c1ce8e4130bd25d1bbb2f81de422
nertsal <nestralnertsal@gmail.com>
2023-12-13 16:19:30 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "HAIID Rasa Bot";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        libDeps = with pkgs; [
+            # zlib
+            # stdenv.cc.cc.lib
+        ];
+        libPath = pkgs.lib.makeLibraryPath libDeps;
+      in with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            python38
+          ];
+          buildInputs = libDeps ++ [
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libPath}"
+          '';
+        };
+      }
+    );
+}
